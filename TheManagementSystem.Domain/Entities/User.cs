b4483c9aa674f2bd3f0e19d62aa09c632d0dc8b4
cfyzgblh <cU2roma@yandex.ru>
2026-07-02 18:57:30 +0300

    feat : domain
    Создать класс пользователя

---
+++ TheManagementSystem.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheManagementSystem.Domain.Enums;
+
+namespace TheManagementSystem.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login {  get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public NotificationType Type { get; set; }
+    }
+}
---
+++ TheManagementSystem.Domain/Enums/NotificationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheManagementSystem.Domain.Enums
+{
+    public enum NotificationType
+    {
+        Disabled, 
+        PushNotifications,
+        ByPhoneNumber,
+        All
+    }
+}
