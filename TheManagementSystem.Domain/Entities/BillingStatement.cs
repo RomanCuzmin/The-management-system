87d10d46c627a9c459352ed1553c1cf255198dda
cfyzgblh <cU2roma@yandex.ru>
2026-07-03 18:58:42 +0300

    Update : Domain
    Создать класс тэга

--- TheManagementSystem.Domain/Entities/BillingStatement.cs
+++ TheManagementSystem.Domain/Entities/BillingStatement.cs
@@ -7,10 +7,14 @@ namespace TheManagementSystem.Domain.Entities
     public class BillingStatement
     {
         public int Id { get; set; }
+        public int WorkerId { get; set; }
+        public int UserId { get; set; }
         public string Accured {  get; set; }
         public string Withheld { get; set; }
         public string PaidOut { get; set; }
         public DateOnly BeginningOfThePeriod { get; set; }
         public DateOnly EndOfThePeriod { get; set; }
+        public Worker Worker { get; set; }
+        public User User { get; set; }
     }
 }
--- TheManagementSystem.Domain/Entities/Budget.cs
+++ TheManagementSystem.Domain/Entities/Budget.cs
@@ -7,8 +7,12 @@ namespace TheManagementSystem.Domain.Entities
     public class Budget
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CategoryId { get; set; }
         public decimal PlannedAmount { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public User User { get; set; }
+        public Category Category { get; set; }
     }
 }
