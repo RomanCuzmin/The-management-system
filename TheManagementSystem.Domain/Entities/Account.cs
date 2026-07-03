1b615933e386b9d8c53f4842ad18f56c7bbee80f
cfyzgblh <cU2roma@yandex.ru>
2026-07-03 15:58:05 +0300

    create : domain : Account
    Создать класс транзакции

---
+++ TheManagementSystem.Domain/Entities/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheManagementSystem.Domain.Enums;
+
+namespace TheManagementSystem.Domain.Entities
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AccountType Type {  get; set; }
+        public decimal InitialBalance { get; set; }
+        public string Currency {  get; set; }
+    }
+}
