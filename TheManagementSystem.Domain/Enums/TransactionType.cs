4886bdfc54f0fbfecc645402a8bdcd759308814a
cfyzgblh <cU2roma@yandex.ru>
2026-07-03 14:43:35 +0300

    create : domain : Transaction
    Создать класс транзакции

---
+++ TheManagementSystem.Domain/Enums/TransactionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheManagementSystem.Domain.Enums
+{
+    public enum TransactionType
+    {
+        Income,
+        Expense,
+        Transfer
+    }
+}
