0128eca5e386e94c5399d3cb51a14ac44daeb359
cfyzgblh <cU2roma@yandex.ru>
2026-07-03 16:28:23 +0300

    create : domain : Budget
    Создать класс бюджета

---
+++ TheManagementSystem.Domain/Entities/Budget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheManagementSystem.Domain.Entities
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public decimal PlannedAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
