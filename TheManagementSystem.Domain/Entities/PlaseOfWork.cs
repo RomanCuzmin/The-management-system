b7f33a8f8309acb980a71089e2579a2a6d504d10
cfyzgblh <cU2roma@yandex.ru>
2026-07-13 19:01:23 +0300

    Create : Domain : PlaseOfWork
    Добавить  класс  места работы

---
+++ TheManagementSystem.Domain/Entities/PlaseOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheManagementSystem.Domain.Entities
+{
+    public class PlaseOfWork
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Company {  get; set; }
+        public string Division { get; set; }
+        public string Post { get; set; }
+        public string ServiceNumber { get; set; }
+        public User User { get; set; }
+    }
+}
