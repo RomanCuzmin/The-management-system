a1b454245ff929def2452629435a6628e5094dd7
cfyzgblh <cU2roma@yandex.ru>
2026-07-13 19:05:30 +0300

    Create : Domain : PersonalData
    Добавить  класс персональных данных

---
+++ TheManagementSystem.Domain/Entities/PersonalData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheManagementSystem.Domain.Entities
+{
+    public class PersonalData
+    {
+        public int Id { get; set; }
+        public int WorkerId { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public Worker Worker { get; set; }
+    }
+}
